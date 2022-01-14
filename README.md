81cacdbcf8ebb4fe17c332bfcf07767cf0994b81
James Spurin <james@spurin.com>
2022-01-14 11:18:45 +0000

    Dockerfile for ubuntu_20.04 with systemd

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# container-systemd
-✨ Popular linux distributions configured with systemd ✨
+## ✨ Popular linux distributions configured with systemd  (ubuntu_20.04)✨
+
+Popular linux distributions with Systemd.  Superb for use with Docker 🐋
+
+## Overview
+
+Source Dockerfile for running Ubuntu 20.04 with systemd in a convenient container.  The image relating to this Dockerfile is available for both amd64 and arm64 on Docker Hub - ```spurin/container-systemd:ubuntu_20.04```
+
+## Examples
+
+Run a systemd container and execute a bash shell, cleanup on exit -
+
+```
+docker run -it --rm --privileged spurin/container-systemd:ubuntu_20.04 /bin/bash
+```
+
+---
+
+Run a systemd container in the background (useful for further customisation, i.e. as a base image
+for another container where additional systemd service files are added) -
+
+```
+CONTAINER=$(docker run -d --privileged spurin/container-systemd:ubuntu_20.04)
+docker exec -it ${CONTAINER} bash
+```
+
+Terminate and Remove -
+
+```
+docker stop ${CONTAINER}
+docker rm ${CONTAINER}
+```
+
+## Build
+
+See the build.sh script for 3 options that can be used for build purposes
+
+1. Build locally
+2. Crossbuild with buildx for amd64 and arm64 (Slow!)
+3. Crossbuild with buildx for amd64 and arm64 using a dedicated instance for alternative cross building (configure accordingly for your remote architecture)
---
+++ container-entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e
+container=docker
+export container
+
+# If a command is provided but a TTY isn't enabled, exit
+if [ $# -ne 0 ] && [ ! -t 0 ]; then
+   echo >&2 'ERROR: TTY needs to be enabled (`docker run -t ...`).'
+   exit 1
+fi
+
+# If a command is provided, configure a systemd service for execution
+if [ $# -ne 0 ]; then
+   env >/etc/container-entrypoint-env
+
+   cat >/etc/systemd/system/container-entrypoint.target <<EOF
+[Unit]
+Description=for container-entrypoint.service
+Requires=container-entrypoint.service systemd-logind.service systemd-user-sessions.service
+EOF
+
+   quoted_args="$(printf " %q" "${@}")"
+   echo "${quoted_args}" >/etc/container-entrypoint-cmd
+
+   cat >/etc/systemd/system/container-entrypoint.service <<EOF
+[Unit]
+Description=container-entrypoint.service
+
+[Service]
+ExecStart=/bin/bash -exc "source /etc/container-entrypoint-cmd"
+# EXIT_STATUS is either an exit code integer or a signal name string, see systemd.exec(5)
+ExecStopPost=/bin/bash -ec "if echo \${EXIT_STATUS} | grep [A-Z] > /dev/null; then echo >&2 \"got signal \${EXIT_STATUS}\"; systemctl exit \$(( 128 + \$( kill -l \${EXIT_STATUS} ) )); else systemctl exit \${EXIT_STATUS}; fi"
+StandardInput=tty-force
+StandardOutput=inherit
+StandardError=inherit
+WorkingDirectory=$(pwd)
+EnvironmentFile=/etc/container-entrypoint-env
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+   systemctl enable container-entrypoint.service
+fi
+
+# Capture the systemd location
+systemd=
+if [ -x /lib/systemd/systemd ]; then
+   systemd=/lib/systemd/systemd
+elif [ -x /usr/lib/systemd/systemd ]; then
+   systemd=/usr/lib/systemd/systemd
+elif [ -x /sbin/init ]; then
+   systemd=/sbin/init
+else
+   echo >&2 'ERROR: systemd is not installed'
+   exit 1
+fi
+
+# Toggle systemd_args whether or not a command is being executed
+if [ $# -ne 0 ]; then
+   systemd_args="--show-status=true --unit=container-entrypoint.target"
+else
+   systemd_args="--show-status=true"
+fi
+
+# Start systemd
+echo "$0: starting $systemd $systemd_args"
+exec $systemd $systemd_args
